3d858e297968af65d500456a2df4a4dfd5536cf8
David Baynard <git@baynard.dev>
2023-10-26 18:05:11 +0100

    Customize devShell configuration

--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
-{ mkGoEnv
+{ lib
 , gomod2nix
+, mkGoEnv
 , mkShell
+, zsh
 }:
 
 let
+  name = "go";
+
   goEnv = mkGoEnv { pwd = ./.; };
 in
 mkShell {
+  inherit name;
+
   packages = [
     goEnv
     gomod2nix
   ];
+
+  shellHook = ''
+    export NIX_SHELL_NAME="${name}"
+    RPROMPT='%F{magenta}${name}%f %1(j.«%j» .)%*'
+    ${zsh}/bin/zsh
+    exit 0
+  '';
 }
