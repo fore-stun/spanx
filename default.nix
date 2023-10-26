7596107785e956c00579fb34975ebb825089d72e
David Baynard <git@baynard.dev>
2023-10-26 17:51:22 +0100

    Configure gomod2nix for spanx

--- default.nix
+++ default.nix
@@ -1,21 +1,15 @@
-{ pkgs ? (
-    let
-      inherit (builtins) fetchTree fromJSON readFile;
-      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
-    in
-    import (fetchTree nixpkgs.locked) {
-      overlays = [
-        (import "${fetchTree gomod2nix.locked}/overlay.nix")
-      ];
-    }
-  )
-, buildGoApplication ? pkgs.buildGoApplication
+{ lib
+, buildGoApplication
 }:
 
 buildGoApplication {
-  pname = "myapp";
+  pname = "spanx";
   version = "0.1";
   pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
+  meta = {
+    license = lib.licenses.mit;
+    mainProgram = "caddy";
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,22 +1,12 @@
-{ pkgs ? (
-    let
-      inherit (builtins) fetchTree fromJSON readFile;
-      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
-    in
-    import (fetchTree nixpkgs.locked) {
-      overlays = [
-        (import "${fetchTree gomod2nix.locked}/overlay.nix")
-      ];
-    }
-  )
-, mkGoEnv ? pkgs.mkGoEnv
-, gomod2nix ? pkgs.gomod2nix
+{ mkGoEnv
+, gomod2nix
+, mkShell
 }:
 
 let
   goEnv = mkGoEnv { pwd = ./.; };
 in
-pkgs.mkShell {
+mkShell {
   packages = [
     goEnv
     gomod2nix
