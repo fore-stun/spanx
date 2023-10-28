edc81ad9877507f9780074944b2f1d60400d0fff
David Baynard <git@baynard.dev>
2023-10-28 05:35:18 +0100

    Include go-mod-graph-chart (gmchart) in devShell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 { lib
+, dex
 , gomod2nix
+, go-mod-graph-chart
+, hostPlatform
 , mkGoEnv
 , mkShell
+, xdg-utils
 , zsh
 }:
 
@@ -9,6 +13,15 @@ let
   name = "go";
 
   goEnv = mkGoEnv { pwd = ./.; };
+
+  BROWSER =
+    if hostPlatform.isLinux then ''
+      ${lib.getExe dex} -d "/run/current-system/sw/share/applications/$(
+        ${xdg-utils}/bin/xdg-settings get default-web-browser
+      )" \
+        | sed -E -n -e 's/^Executing command: //p'
+    ''
+    else throw "Untested";
 in
 mkShell {
   inherit name;
@@ -16,10 +29,12 @@ mkShell {
   packages = [
     goEnv
     gomod2nix
+    go-mod-graph-chart
   ];
 
   shellHook = ''
     export NIX_SHELL_NAME="${name}"
+    export BROWSER="$(${BROWSER})"
     RPROMPT='%F{magenta}${name}%f %1(j.«%j» .)%*'
     ${zsh}/bin/zsh
     exit 0
